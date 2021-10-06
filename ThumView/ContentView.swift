2524bcfab76cdbff505916ed654e6f8b2c621fd1
Jrscodes <thelions20k@icloud.com>
2021-10-05 22:29:39 -0600

    Initial Commit

---
+++ ThumView/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ThumView
+//
+//  Created by Abraham on 10/5/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ThumView/ThumViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThumViewApp.swift
+//  ThumView
+//
+//  Created by Abraham on 10/5/21.
+//
+
+import SwiftUI
+
+@main
+struct ThumViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
